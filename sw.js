36a4252af93901752a525694132de6ae6d55e84a
Rene <peppersje98@gmail.com>
2026-05-27 10:01:48 +0900

    Fix iPad default blue text

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for PWA — Network first, cache fallback
-const CACHE_NAME = 'mahjong-app-v9';
+const CACHE_NAME = 'mahjong-app-v10';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
